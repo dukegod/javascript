0e648ddd84e125b65f964f54436c982864e7d4ee
duke <841530656@qq.com>
2015-05-21 15:17:10 +0800

    test

--- es6.js
+++ es6.js
@@ -1,3 +1,14 @@
-for (let i = 0; i < 3; i++) {
-    console.log('i:', i);
+var funcs = [];
+
+function createfunc(i) {
+    return function() { console.log("My value: " + i); };
 }
+
+for (var i = 0; i < 3; i++) {
+    funcs[i] = createfunc(i);
+}
+
+for (var j = 0; j < 3; j++) {
+    funcs[j]();                       
+}
+
