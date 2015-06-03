5fd88d4d1054329a216e4abf599d11eeafe245a8
dukegodPC <dukehui1991@gmail.com>
2015-06-03 23:13:04 +0800

    ecma

--- ecma6/esbljg.js
+++ ecma6/esbljg.js
@@ -1,8 +1,52 @@
 // 变量结构
 
-var s = "𠮷a";
+function do_something() {
+  // console.log(foo); // ReferenceError
+  let foo = 2;
+}
 
-s.codePointAt(0) // 134071
-s.codePointAt(1) // 57271
+ do_something();
 
-s.charCodeAt(2) // 97
+
+ for(let i=0; i<7; i++ ){
+ 	// console.log(i);
+ }
+
+ // var [a, b, c] = [1, 2, 3];
+ // console.log(a);
+
+// function example() {
+//   return [1, 2, 3];
+// }
+// var [a, b, c] = example();
+
+// console.log(b);
+
+function example() {
+  return {
+    foo: 1,
+    bar: 2
+  };
+}
+var { foo, bar } = example();
+console.log(foo);
+
+
+var person = {
+	a1: 'du',
+	b1: 18,
+	c1: 'beijing china'
+}
+
+let { a1, b1, c1 } = person;
+console.log(a1, b1, c1);
+
+var jsonData = {
+  id: '42',
+  status: "OK",
+  data: [867, 5309]
+}
+
+let { id, status, data: number } = jsonData;
+
+console.log(id, status, number)
