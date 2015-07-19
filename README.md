9002fff32b17751a2ab9212c1037bac4cc3e3e9e
dukegodPC <dukehui1991@gmail.com>
2015-07-20 00:51:23 +0800

    720

--- README.md
+++ README.md
@@ -1,24 +1,78 @@
-主要是用来测试的
+## javascript  
 
-scope.js:
-用来测试函数的生成方式：
-	1 函数声明式
-	2 函数表达式
+### 函数
 
-closure.js:
-用来测试闭包的js：
-	1，javascript 中每一个函数都是一个闭包
-	2，闭包特性：当一个函数返回它内部定义的一个函数时，就产生一个闭包，闭包不但包括被返回的函数。还包括这个函数的定义环境
-	3，函数的每一个实例化都是相互独立的	
-oop.js:
-	主要是讲解创建对象问题	
-oppExtent.js:
-	js继承问题
++ func1.js
 
-index.html:
-	入口
-jsmodel.js:
-	js设计模型浅浅的探究	
+> 函数部分开始样式，构造函数，封装，继承，多态 
+
+
+### 对象
+
+一种重要的数据类型，自包含的数据集合，包含在对象中的数据可以通过两种形式访问：
+
+1, 属性(property)
+2, 方法(method)
+
+对象可以看做有一些属性和方法一起构成的一个实体对象。
+
+对象类型：
+
+1, 内建对象
+
+	既是数组对象，时间对象...
+
+2, 宿主对象(host object)
+
+	既是它运行环境的对象，一般就是浏览器对象
+
+对象的创建
+
+1， new object()
+
+	var person = new Object();
+
+	person.name = 'duke';
+	person.age = 18;
+
+	console.log(person.name);	
+
+2, 对象字面量表示法(个人比较喜欢的方式，json对象)
+
+	var p ={
+		name: 'liuhui',
+		age: 99,
+		say: function(){
+			console.log(this);
+			return '6666给外部的调用返回一个数值';
+		}
+	}
+	console.log(p.name);
+	console.log(p.say()+'out');	
+
+	return，返回一个数值，这样外部函数才能达到，也就是闭包的用法，以后再详细说明
+
+
+## 原型链
+
+
+
+## this 
+
++ 闭包总指向上一级
++ 构造函数中，指向本身
++ 引用时候，指向Windows
+
+
+
+
+
+
+
+
+
+
+
 
 
 
---
+++ func2.js
@@ -0,0 +1,85 @@
+//函数可以用作值进行传递
+
+function addFunc(f,n){
+	return f(n);
+}
+
+function add(n){
+	return n+7;
+}
+
+var ins = addFunc(add,3);
+console.log(ins);
+
+
+
+//构造函数才需要实例化
+function Goufunc(x,y){
+	this.x= x;
+	this.y= y;
+	console.log(x+':'+y);
+}
+
+var s= new Goufunc(34,10);
+console.log(s);
+
+
+//方法调用模式
+var myObj = {//对象字面量
+    param1: 1,
+    param2: 2,
+    sum: function (){
+	//this关键字只带当前的对象
+	return this.result = this.param1 + this.param2; 
+    }
+}
+myObj.sum(); 
+
+
+//函数调用模式
+var add = function(a, b){
+    return a + b;
+}
+add(1,2);  
+//等同于
+function add(a, b){
+    return a + b;
+}
+add(1,2);//=>3
+
+
+//构造器调用模式
+var add = function() {
+    this.name = "汇智网";
+    this.sum = function (a, b){
+        return a + b;
+    }
+}
+var obj = new add(); //obj是一个对象
+obj.sum(1,2);  //=>3
+
+
+//apply调用模式
+//JavaScript中函数也是对象，也可以拥有方法。其中call()和apply()用来间接的调用函数。称之为apply调用模式。
+//apply方法让我们构建一个参数数组传递给调用函数。它也允许我们选择this的值。
+// apply方法接收两个参数，第一个是要绑定给this的值，第二个就是一个参数数组。
+//语法：函数名.apply(对象, 参数数组);
+
+//示例：
+
+var add = function (a, b) {
+    return a + b;
+}
+ 
+add.apply(null,[1,2]);  //=>3
+
+//call方法与apply方法类似，区别在call第二参数不是一个数组而是需要列举出来。
+//比如上述中的代码用call方法来实现，如下
+
+var add = function (a, b) {
+    return a + b;
+}
+add.call(null,1,2); //=>3
+
+
+
