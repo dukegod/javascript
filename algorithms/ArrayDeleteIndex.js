d2d7c669a4f6e78e46b3ac04e2f5eba65d7087e5
liuhui <dukehui1991@gmail.com>
2016-04-19 14:34:02 +0800

    数组按照下标输出指定数据

---
+++ algorithms/ArrayDeleteIndex.js
@@ -0,0 +1,10 @@
+// ArrayDeleteIndex.js
+Array.prototype.indexRemove = function(num)
+　{
+　　if(isNaN(num)||num>this.length){return false;}
+　　this.splice(num,1);
+　}
+
+var arr = ["a","b","c","d"];
+arr.indexRemove(2);
+console.log(arr);
