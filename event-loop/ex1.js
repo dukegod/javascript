5cc9b61f14a6ef599a5858b82492fed0a3031546
dukegod <dukehui1991@gmail.com>
2018-03-27 19:46:47 +0800

    事件机制

---
+++ event-loop/ex1.js
@@ -0,0 +1,25 @@
+console.log('main1');
+process.nextTick(function() {
+    console.log('process.nextTick1');
+});
+setTimeout(function() {
+    console.log('setTimeout');
+    process.nextTick(function() {
+        console.log('process.nextTick2');
+    });
+}, 0);
+new Promise(function(resolve, reject) {
+    console.log('promise');
+    resolve();
+}).then(function() {
+    console.log('promise then');
+});
+console.log('main2');
+
+// main1
+// promise
+// main2
+// process.nextTick1
+// promise then
+// setTimeout
+// process.nextTick2
