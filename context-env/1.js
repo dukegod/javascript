f5b21e53522216b7fbca9c822be17b742ba962e9
dukegod <dukehui1991@gmail.com>
2018-03-28 19:01:14 +0800

    context

---
+++ context-env/1.js
@@ -0,0 +1,19 @@
+var x = 1, y =2;
+
+function z() {
+    var x = 2;
+    return {
+        x: x,
+        y: function (a,b) {
+            x = a+b
+        },
+        z: function () {
+            return x;
+        }
+    }
+}
+
+a = z();
+a.y(x,y)
+
+console.log(a.z(), a.x, x)
