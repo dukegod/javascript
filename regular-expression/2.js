fc7b50d66027804ea35b41bcfc98aecf0a039648
dukegod <dukehui1991@gmail.com>
2018-05-15 16:12:47 +0800

    test reg

---
+++ regular-expression/2.js
@@ -0,0 +1,21 @@
+const regex = /sa./g;
+const str = `sal.js
+sala.js
+salass.js
+sa2.js
+sa.js
+da.js
+ad.js`;
+let m;
+
+while ((m = regex.exec(str)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+    
+    // The result can be accessed through the `m`-variable.
+    m.forEach((match, groupIndex) => {
+        console.log(`Found match, group ${groupIndex}: ${match}`);
+    });
+}
