d80b77011d180b8ae88e7aabc39e31fe2f4ff8f6
dukegod <dukehui1991@gmail.com>
2018-06-14 18:04:21 +0800

    reform

--- context-env/1.js
+++ context-env/1.js
@@ -1,19 +1,20 @@
-var x = 1, y =2;
+
+var x = 1, y = 2;
 
 function z() {
-    var x = 2;
-    return {
-        x: x,
-        y: function (a,b) {
-            x = a+b
-        },
-        z: function () {
-            return x;
-        }
+  var x = 2;
+  return {
+    x: x,
+    y: function(a, b) {
+      x = a + b;
+    },
+    z: function() {
+      return x;
     }
+  };
 }
 
 a = z();
-a.y(x,y)
+a.y(x, y);
 
-console.log(a.z(), a.x, x)
+console.log(a.z(), a.x, x);
