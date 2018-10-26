01cd108289cc054970512a3da14a455d6308e1e5
dukegod <dukehui1991@gmail.com>
2018-10-26 19:11:02 +0800

    zigzag-conversion

---
+++ leetCode/6-zigzag-conversion/README.md
@@ -0,0 +1,9 @@
+将字符串 "PAYPALISHIRING" 以Z字形排列成给定的行数：
+
+P   A   H   N   
+A P L S I I G   
+Y   I   R   
+
+之后从左往右，逐行读取字符："PAHNAPLSIIGYIR"
+
+实现一个将字符串进行指定行数变换的函数:
---
+++ leetCode/6-zigzag-conversion/index.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @param {number} numRows
+ * @return {string}
+ */
+const convert = function(s, numRows) {
+  if (numRows===1) return s;
+  
+};
