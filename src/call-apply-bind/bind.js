098b2950f8861f1559db3a1487d218b359cf1be0
dukegod <dukehui1991@gmail.com>
2018-12-20 19:24:59 +0800

    bind 代码实现

---
+++ src/call-apply-bind/bind.js
@@ -0,0 +1,64 @@
+Function.prototype.bindWheel = function(oThis) {
+  //  判断调用者是不是函数
+  if (typeof this !== 'function') {
+    // internal IsCallable function
+    throw new TypeError(
+      'Function.prototype.bind - what is trying to be bound is not callable'
+    );
+  }
+
+  //  手里参数
+  var aArgs = Array.prototype.slice.call(arguments, 1);
+
+  // 调用者缓存
+  var fToBind = this;
+
+  // 空函数
+  var fNOP = function() {};
+
+  //
+  var fBound = function() {
+    // this instanceof fNOP === true时,说明返回的fBound被当做new的构造函数调用
+    //  这个类型检测比较重要
+    return fToBind.apply(
+      this instanceof fNOP ? this : oThis,
+      // 获取调用时(fBound)的传参.bind 返回的函数入参往往是这么传递的
+      aArgs.concat(Array.prototype.slice.call(arguments))
+    );
+  };
+
+  // 维护原型关系
+  if (this.prototype) {
+    // Function.prototype doesn't have a prototype property
+    fNOP.prototype = this.prototype;
+  }
+  // 下行的代码使fBound.prototype是fNOP的实例,因此
+  // 返回的fBound若作为new的构造函数,new生成的新对象作为this传入fBound,新对象的__proto__就是fNOP的实例
+  fBound.prototype = new fNOP();
+
+  return fBound;
+};
+
+
+
+// test
+
+function Point(x, y) {
+  this.x = x;
+  this.y = y;
+}
+
+Point.prototype.toString = function() {
+  return this.x + ',' + this.y;
+};
+
+var p = new Point(1, 2);
+p.toString(); // '1,2'
+var emptyObj = {};
+var YAxisPoint = Point.bind(emptyObj, 0);
+
+var axisPoint = new YAxisPoint(5);
+axisPoint.toString(); // '0,5'
+axisPoint instanceof Point; // true
+axisPoint instanceof YAxisPoint; // true
+new Point(17, 42) instanceof YAxisPoint; // true
