2db0be7f45f4e5a45338a181df5e2996e068315b
dukegod <dukehui1991@gmail.com>
2019-01-14 14:35:52 +0800

    destructure

---
+++ src/destructure/destructure-modules/index.js
@@ -0,0 +1,6 @@
+import vars as {header, middle, last} from './var.js';
+
+// console.log(vars);
+
+// const {header, middle, last} = vars;
+console.log(header)
---
+++ src/destructure/destructure-modules/var.js
@@ -0,0 +1,9 @@
+const vars = {
+  header: {},
+  middle: {
+    one: 1
+  },
+  last: [{},{}]
+}
+
+export default vars;
