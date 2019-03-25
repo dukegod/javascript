51c36255c55ec5e80f2d4c2ddabf618bd08b5f66
dukegod <dukehui1991@gmail.com>
2019-03-25 22:43:01 +0800

    leetcode

--- leetCode/14-longest-common-prefix/index.js
+++ leetCode/14-longest-common-prefix/index.js
@@ -7,5 +7,30 @@
  * @return {string}
  */
 const longestCommonPrefix = function(strs) {
-  const commandStrs = '';
+  let minLength;
+  let len;
+  let commandStrs;
+  len = strs[0].length;
+  commandStrs = strs[0];
+  for (let i = 1; i < strs.length; i++) {
+    if (strs[i].length < len) {
+      minLength = i;
+      len = strs[i].length;
+      commandStrs = strs[i];
+    }
+  }
+
+  // console.log(len, minLength, commandStrs);
+  while (len) {
+    let str = commandStrs.slice(0, len);
+    // console.log(str);
+    if( strs.every(item => item.indexOf(str) !== -1)) {
+      return str
+    }
+    len --;
+  }
+
 };
+
+
+longestCommonPrefix(["flower","flow","flight", "flo"]);
