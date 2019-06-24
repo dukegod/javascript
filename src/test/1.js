147e06932b346e6586e3fd29406230df0e387705
dukegod <dukehui1991@gmail.com>
2019-06-24 18:35:39 +0800

    test

--- src/test/1.js
+++ src/test/1.js
@@ -1,9 +1,11 @@
-var a = {n:1}
-var b = a;
-a.x = a
-a = {n:2}
+var a = {
+  n:1,
+  x : 90
+}
+// var b = a;
+a.x = b = a = {n:2}
 
 console.log(a) // {n:2}
 console.log(a.x) // {n:2}
 console.log(b)  // {n:1}
-console.log(b.x) // undefined
+// console.log(b.x) // undefined
