43bbb90e1b8da3660c846ab5b762a8c61f516289
dukegod <dukehui1991@gmail.com>
2019-07-04 17:34:59 +0800

    字符串判断空处理

--- components/is-string-empty/index.js
+++ components/is-string-empty/index.js
@@ -1,10 +1,15 @@
-
+/**
+ * @description 判断字符串是不是为空, 需要去除 null， undefined， {}
+ * @param params
+ * @returns {boolean}
+ */
 function isStringEmpty(params) {
-  console.log( typeof params);
-  console.log(params.length);
-  console.log(params.length === 0);
-  return params.trim().length === 0;
+  return typeof params === 'string' ? params.trim().length === 0 : false;
 }
 
-
-isStringEmpty('')
+console.log(isStringEmpty('   ddd'));
+console.log(isStringEmpty(''));
+console.log(isStringEmpty());
+console.log(isStringEmpty(null));
+console.log(isStringEmpty(undefined));
+console.log(isStringEmpty({}));
