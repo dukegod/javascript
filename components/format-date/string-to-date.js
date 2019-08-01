2e676d7f74e02c39cf9d50553e28b489763360fa
dukegod <dukehui1991@gmail.com>
2019-08-01 22:55:52 +0800

    time to object

--- components/format-date/string-to-date.js
+++ components/format-date/string-to-date.js
@@ -4,15 +4,24 @@
  */
 function stringToDate(s) {
   const date = new Date(s);
+  const year = date.getFullYear()
+  const Mouth = date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1
+  const Day = date.getDate() + 1 < 10 ? '0' + date.getDate() : date.getDate()
+  const Hour = date.getHours()
+  const Minutes = date.getMinutes()
+  const Second = date.getSeconds() + 1 < 10 ? '0' + date.getSeconds() : date.getSeconds()
+  const ymd = `${year}-${Mouth}-${Day}`
+
   return {
-    year: date.getFullYear(),
-    Mouth: date.getMonth() + 1 < 10 ? '0' + (date.getMonth() + 1) : date.getMonth() + 1,
-    Day: date.getDate() + 1 < 10 ? '0' + date.getDate() : date.getDate(),
-    Hour: date.getHours(),
-    Minutes: date.getMinutes(),
-    Second: date.getSeconds() + 1 < 10 ? '0' + date.getSeconds() : date.getSeconds()
-  };
+    year,
+    Mouth,
+    Day,
+    Hour,
+    Minutes,
+    Second,
+    ymd
+  }
 }
 
-export default stringToDate;
+// export default stringToDate;
 console.log(stringToDate(1553500088264));
